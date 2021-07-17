b5ed10209e4aa4a02b14c38d56164b6a91e5cd46
Ripu Sudan Soni <ripu7481@gmail.com>
2021-07-17 11:18:48 +0530

    first commit

---
+++ src/components/IndianCities.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function IndianCities() {
+
+    const cityList = [{ name: 'Goa', country: 'India' },
+    { name: 'Amsterdam', country: 'Netherlands' },
+    { name: 'New York', country: 'USA' },
+    { name: 'Darjeeling', country: 'India' },
+    { name: 'Tokyo', country: 'Japan' },
+    { name: 'Lonavala', country: 'India' },
+    { name: 'Brandenburg Gate', country: 'Germany' },
+    { name: 'Reichstag Building', country: 'Germany' },
+    { name: 'Museum Island', country: 'Germany' },
+    { name: 'Munnar', country: 'India' },
+    { name: 'Leh Ladakh', country: 'India' },
+    { name: 'Goa', country: 'India' },
+    { name: 'Agra', country: 'India' },
+    { name: 'Dalhousie', country: 'India' },
+    { name: 'Coorg', country: 'India' },
+    { name: 'Rome', country: 'Italy' },
+    { name: 'Milan', country: 'Italy' },
+    { name: 'Venice', country: 'Italy' },
+    { name: 'Varanasai', country: 'India' },
+    { name: 'Jaipur', country: 'India' },
+    { name: 'The Hofburg', country: 'Austria' },
+    { name: 'Belvedere Palace', country: 'Austria' },
+    { name: 'St. Stephen Cathedral', country: 'Austria' },
+    { name: 'Kahna National Park', country: 'India' },
+    { name: 'Amritsar', country: 'India' },
+    { name: 'Mussoorie', country: 'India' },
+    { name: 'Mount Abu', country: 'India' },
+    { name: 'Tirupati', country: 'India' },
+    ]
+    const filteredCities = cityList.filter((city) => {
+        return city.country == 'India';
+    });
+    console.log(filteredCities);
+    console.log("/////////////////");
+
+
+    let count = 0;
+    let city;
+    const allcity = filteredCities.map((obj) => {
+        city = obj.name;
+        console.log(city);
+        ++count;
+        let keyvalue = `location${count}`;
+        return (<li key = {keyvalue}>{city}</li>)
+        
+    });
+    console.log(allcity);
+    console.log(`location${count}`);
+
+    return (
+        <>
+        {allcity}
+        </>
+    ); 
+    
+}
+export default IndianCities;
